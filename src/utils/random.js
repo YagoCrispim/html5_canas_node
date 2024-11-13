ee5d5435b3c4c2647a801eaa1b3ac74c37f05541
Yago Crispim <34103098+YagoCrispim@users.noreply.github.com>
2024-11-13 08:10:59 -0300

    html5 canvas node

---
+++ src/utils/random.js
@@ -0,0 +1,14 @@
+/**
+ * Generates a random identifier using the Web Crypto API.
+ * The identifier is created from a random byte array and converted to a string.
+ *
+ * @returns {string} A random identifier consisting of 5 bytes, with commas removed.
+ * 
+ * @todo -> Better and faster impl
+ */
+export function randomId() {
+  const buf = new Uint8Array(5);
+  window.crypto.getRandomValues(buf);
+  const id = buf.toString();
+  return id.replaceAll(",", "");
+}
